8017ec47d1b6b7120c09e64a95868644d8e25585
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-12-05 04:27:52 +0530

    4th dump

--- src/OOPS/Op1.java
+++ src/OOPS/Op1.java
@@ -1,5 +1,17 @@
 package OOPS;
 
 public class Op1 {
+int age;
+String name;
 
+    public static void main(String[] args) {
+        Op1 mode = new Op1();
+        mode.age = 24;
+        mode.name = "Mecha";
+
+        int nage = mode.age;
+        String alias = mode.name;
+        System.out.println(nage );
+        System.out.println( alias);
+    }
 }
