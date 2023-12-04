2c9d7433a34d2f21b12cc8e3bd811d679ae4e4ef
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-12-05 04:37:15 +0530

    5th dump

---
+++ src/OOPS/Driver.java
@@ -0,0 +1,12 @@
+package OOPS;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        Op2 mode = new Op2();
+
+        mode.Op2("Deva", 24);
+
+        System.out.println("The name is "+ mode.getName() + " and The age is "+ mode.getAge());
+    }
+}
---
+++ src/OOPS/Op2.java
@@ -0,0 +1,20 @@
+package OOPS;
+
+public class Op2 {
+
+    private int age;
+    private String name;
+
+    public void Op2(String name, int age){
+       this.name = name;
+       this.age  = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+}
