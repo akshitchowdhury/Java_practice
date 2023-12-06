8d66a08508bf826f3409174bbe59e176196e2f34
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-12-06 19:51:40 +0530

    while4

---
+++ src/Basics/even_num.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class even_num {
+
+    public static void main(String[] args) {
+
+        int num = 1;
+
+        while(num<=20){
+
+            if(num%2==0){
+                System.out.println(num);
+            }
+            num++;
+        }
+    }
+
+}
