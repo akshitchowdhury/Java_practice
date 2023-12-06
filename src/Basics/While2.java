7ff51324a80fa47cd0cecf77cb39e626e1deea65
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-12-06 19:32:18 +0530

    while2

--- src/Basics/While2.java
+++ src/Basics/While2.java
@@ -1,5 +1,19 @@
 package Basics;
 
 public class While2 {
+    public static void main(String[] args) {
+
+        //print number from-1 to -10 using while loop
+
+        int num = -1;
+
+        while(num>=-10){
+            System.out.println(num);
+            num--;
+        }
+
+        System.out.println("Loop done");
+
+    }
 
 }
