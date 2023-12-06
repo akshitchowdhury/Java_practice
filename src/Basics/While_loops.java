c72145659aeb3281a00a79b64e5e92a430210004
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-12-06 19:29:45 +0530

    while1

---
+++ src/Basics/While_loops.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class While_loops {
+    public static void main(String[] args) {
+
+        //print 1 to 10
+
+        int num = 1;
+
+        while(num<=10){
+
+            System.out.println(num);
+            num++;
+        }
+    }
+}
