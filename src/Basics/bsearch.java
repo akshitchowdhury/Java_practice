166b5a31268bc04f4e25233a3c51ccb93f5189aa
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-12-07 19:18:28 +0530

    search_log

---
+++ src/Basics/bsearch.java
@@ -0,0 +1,42 @@
+package Basics;
+
+public class bsearch {
+
+
+    public static int target(int num, int arr[]){
+
+
+        int start = 0;
+        int end = arr.length-1;
+
+
+        while(start<=end){
+            int mid = start+(end-start)/2;
+
+            if(num<arr[mid]){
+
+                end = mid-1;
+            }
+
+            else if(num>arr[mid]){
+                start= mid+1;
+            }
+
+            else{
+                return mid;
+            }
+
+                    }
+        return -1;
+
+
+    }
+    public static void main(String[] args) {
+
+        int num = 100;
+        int arr[] = {10,12,35,43,50,100};
+
+        System.out.println(target(num, arr));
+
+    }
+}
