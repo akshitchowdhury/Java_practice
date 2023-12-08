f28ffdc2d5c52f646d84d14ff9e46fa1982f4847
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-12-08 17:54:19 +0530

    sort2

--- src/Sorting/Selection_sort.java
+++ src/Sorting/Selection_sort.java
@@ -12,7 +12,7 @@ static int[] s_sort(int arr[]){
     for(int i=0; i<arr.length-1; i++){
 
         int mIx = i;
-        for(int j =mIx+1; j<arr.length; j++ ){
+        for(int j =i+1; j<arr.length; j++ ){
 
             if(arr[j]<arr[mIx]){
 
--- src/Sorting/Sort2.java
+++ src/Sorting/Sort2.java
@@ -1,4 +1,35 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class Sort2 {
+    //sort a list of numbers in ascending order
+
+    public static void main(String[] args) {
+
+        int arr[] = {3,54,1220,45,2,10,809};
+        System.out.println(Arrays.toString(sorter(arr)));
+
+    }
+
+
+    static int[] sorter(int[] arr){
+
+        for(int i =0; i<arr.length-1;i++){
+            int Ix = i;
+
+            for(int j= i+1; j<arr.length;j++){
+
+                if(arr[j]<arr[Ix]){
+                    Ix=j;
+                }
+            }
+
+            int temp = arr[Ix];
+            arr[Ix] = arr[i];
+            arr[i] = temp;
+        }
+
+        return arr;
+    }
 }
